55c2e15f8e27f85db78b6627acbbba2929808529
George <georgeroscoe@hotmail.com>
2022-12-08 00:08:31 +0100

    Simplify

--- main.py
+++ main.py
@@ -32,11 +32,11 @@ def scrape_badi():
     driver.get(badi_link)
     badi_page = driver.current_url
 
-    room_list = driver.find_elements("xpath", "//div[@id]")
-    regex_pattern = re.compile(r"^list-room-card-*")
+    room_list = driver.find_elements("xpath", '//div[starts-with(@id,"list-room-card")]')
+    # regex_pattern = re.compile(r"^list-room-card-*")
     logging.info(room_list)
     for element in room_list:
-        if regex_pattern.search(element.get_attribute("id")):
+        # if regex_pattern.search(element.get_attribute("id")):
             room_link = element.find_element(By.CSS_SELECTOR, 'a[data-qa="room-card-link"]').get_attribute('href')
             logging.info(room_link)
             room_link_cleaned = re.sub(r'\?.*', '', room_link)
@@ -82,4 +82,4 @@ def check_and_add(item, database):
 
 while True:
     scrape_badi()
-    time.sleep(60)
+    time.sleep(10)
